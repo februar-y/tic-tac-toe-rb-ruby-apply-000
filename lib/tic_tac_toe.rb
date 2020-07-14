d92793bfd146f09edb2a55f3ef8b5dc29ad9271d
rational-architecture-1641 <jarmah241@gmail.com>
2020-07-14 13:39:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -84,3 +84,30 @@ def won?(board)
   return false
 end
 
+def full?(board)
+  board.all?{|x| x == "X" || x == "O"}
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+  if won?(board)
+    return board[won?(board)[0]]
+  else
+    return nil
+  end
+end
