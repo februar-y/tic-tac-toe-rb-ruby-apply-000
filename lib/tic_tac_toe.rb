3eb889e941c5a94a9230ef96b814f78106f7395e
rational-architecture-1641 <jarmah241@gmail.com>
2020-07-14 14:05:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -118,7 +118,7 @@ def play(board)
   end
   
   if won?(board)
-    "Congratulations #{winner(board)}!"
+    return "Congratulations #{winner(board)}!"
   elsif draw?(board)
     puts "Cat's Game!"
   end
