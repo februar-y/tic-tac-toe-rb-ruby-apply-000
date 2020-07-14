3bad780dd93dc1f84fdde22aef6b0d7cb32b195e
rational-architecture-1641 <jarmah241@gmail.com>
2020-07-14 13:33:56 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,15 @@
+#Helper methods
+
+#win combo constant
+WIN_COMBINATIONS = [
+  [0,1,2], #top row
+  [3,4,5], #mid row
+  [6,7,8], #bottom row
+  [0,3,6], #left column
+  [1,4,7], #mid column
+  [2,5,8], #right column
+  [0,4,8], # diag 1
+  [2,4,6]  #diag 2
+]
+
+#Display board method
