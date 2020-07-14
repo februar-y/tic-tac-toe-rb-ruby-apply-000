11a1720fd6aeb3f03bf0fd8147aadec696748819
rational-architecture-1641 <jarmah241@gmail.com>
2020-07-14 13:34:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,3 +13,11 @@ WIN_COMBINATIONS = [
 ]
 
 #Display board method
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
